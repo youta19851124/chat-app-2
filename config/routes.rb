7d2803908fd095e8b0b6b1e962ee4d34e4f3f705
youta19851124 <youta19851124@gmail.com>
2023-07-27 19:47:43 +0900

    新規作成画面の表示

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   get 'messages/index'
   root to: "messages#index"
-    resources :users, only: [:edit, :update]
-    resources :rooms, only: [:new, :create]
+  resources :users, only: [:edit, :update]
+  resources :rooms, only: [:new, :create]
 end
